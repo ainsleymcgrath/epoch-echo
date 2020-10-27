c019d931133ba250d64c930d37268efb3a866060
Ainsley <ainsley@amper.xyz>
2020-10-26 22:50:06 -0500

    move scripts to their own home, enforce changelog

---
+++ scripts/changelog-did-change.sh
@@ -0,0 +1,18 @@
+# did the changelog change? for enforcing documentation on deployed branches
+branch=$(git branch --show-current)
+
+if [[ $branch == 'master' || $branch == 'dev' ]]; then
+  git diff --name-only \
+    | grep 'CHANGELOG' \
+    | wc -l \
+    | awk '{
+      if ($0 == 1) {
+        print "Ok"; exit 0
+      } else {
+        print ">:( Changelog"; exit 1
+      }
+    }'
+  exit  # awk's exit doesn't exit the whole script
+fi
+
+echo "Didn't check changelog fyi."
