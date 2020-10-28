821198882d0fcbe3812f02e0c08775eb87bf5011
Ainsley <ainsley@amper.xyz>
2020-10-27 21:15:58 -0500

    move state stuff into separate module

---
+++ ee_cli/content.py
@@ -0,0 +1,117 @@
+"""Dispatch actions that determine the user-facing content.
+
+`dispatch` and `visible_content` are the major players here.
+All other functions are in service of affecting state."""
+from textwrap import indent
+from typing import Callable
+
+import pyperclip
+import typer
+
+from ee_cli.constants import (
+    CONFIGURATION_INFO,
+    COPY_HOTWORDS,
+    DROP_HOTWORDS,
+    EXIT_HOTWORDS,
+    GO_BACK_HOTWORDS,
+    HELP_HEADER,
+    HELP_HOTWORDS,
+    HOTWORDS_HELP,
+    NO_TIMES_YET_MESSAGE,
+    RESET_HOTWORDS,
+    SHOW_CONFIG_HOTWORDS,
+    TOGGLE_INDEX_HOTWORDS,
+)
+from ee_cli.ui import OptionallyLatentString, TransformedUserInputStore, make_dispatcher
+from ee_cli.utils import flip_time_format
+
+# UI state data is stored in these variables
+user_inputs = TransformedUserInputStore()
+help_ = OptionallyLatentString(HELP_HEADER + indent(HOTWORDS_HELP, "   "))
+config = OptionallyLatentString(HELP_HEADER + indent(CONFIGURATION_INFO, "   "))
+
+
+# the following functions affect state
+def toggle_index():
+    """Show/hide the index of transformed items in the repl."""
+    user_inputs.show_index = not user_inputs.show_index
+
+
+def clear_list():
+    """Clear all transformations from the repl."""
+    user_inputs.clear()
+
+
+def drop_list_item(*indexes):
+    """Drop the value at any of the specifed indexes.
+    Drop [-1] if no index provided."""
+    if not indexes:
+        user_inputs.pop()
+        return
+
+    user_inputs._working_list = [
+        item
+        for idx, item in enumerate(user_inputs._working_list)
+        if str(idx) not in indexes
+    ]
+
+
+def append_to_list(item):
+    """Add an item to the store for transformation."""
+    user_inputs.append(item)
+
+
+def quit():
+    """Leave the repl.
+
+    As of this moment, state needs to be cleaned up on exit in service of testing.
+    During the lifecycle of test invocation, the state variables stick around across
+    runs for some reason."""
+    user_inputs.clear()
+    help_.latent = True
+    config.latent = True
+    raise typer.Exit
+
+
+def show_help():
+    """Make help visible by toggling the latent property of `help_`."""
+    help_.latent = not help_.latent
+
+
+def show_config():
+    """Make your configuration visible by toggling the latent property on `config`."""
+    config.latent = not config.latent
+
+
+def go_back():
+    """Make all latent strings latent again so they do not show on screen."""
+    for string in [config, help_]:
+        string.latent = True
+
+
+def copy_to_clipboard():
+    """Copy all visible conversion results to the clipboard."""
+    values = "\n".join(map(flip_time_format, user_inputs._working_list))
+    pyperclip.copy(values)
+    typer.echo(
+        f"Converted date{'s' if len(user_inputs) > 1 else ''} copied to clipboard."
+    )
+    quit()
+
+
+dispatch: Callable[[str], None] = make_dispatcher(
+    [RESET_HOTWORDS, clear_list],
+    [DROP_HOTWORDS, drop_list_item],
+    [TOGGLE_INDEX_HOTWORDS, toggle_index],
+    [EXIT_HOTWORDS, quit],
+    [HELP_HOTWORDS, show_help],
+    [SHOW_CONFIG_HOTWORDS, show_config],
+    [GO_BACK_HOTWORDS, go_back],
+    [COPY_HOTWORDS, copy_to_clipboard],
+    default=append_to_list,
+)
+
+
+def visible_content():
+    """Return the first string-ish object that is not falsey at the moment."""
+    return next(x for x in [help_, config, user_inputs, NO_TIMES_YET_MESSAGE] if len(x))
--- ee_cli/main.py
+++ ee_cli/main.py
@@ -1,7 +1,6 @@
 """Entrypoint for `ee`.
 Functions not decorated with @app are actions to affect state"""
 import readline  # noqa: F401
-from textwrap import indent
 from typing import List
 
 import pyperclip
@@ -9,111 +8,12 @@ import typer
 from click import clear
 
 from ee_cli import __version__
-from ee_cli.constants import (
-    CONFIGURATION_INFO,
-    COPY_HOTWORDS,
-    DROP_HOTWORDS,
-    EXIT_HOTWORDS,
-    GO_BACK_HOTWORDS,
-    HELP_HEADER,
-    HELP_HOTWORDS,
-    HOTWORDS_HELP,
-    NO_TIMES_YET_MESSAGE,
-    RESET_HOTWORDS,
-    SHOW_CONFIG_HOTWORDS,
-    TOGGLE_INDEX_HOTWORDS,
-)
-from ee_cli.ui import OptionallyLatentString, TransformedUserInputStore, make_dispatcher
+from ee_cli.constants import HOTWORDS_HELP
+from ee_cli.content import dispatch, visible_content
+from ee_cli.ui import TransformedUserInputStore
 from ee_cli.utils import flip_time_format
 
 app = typer.Typer(name="ee", help="A salve for timesmiths 🧴🕰️")
-
-
-# UI state data is stored in these variables
-user_inputs = TransformedUserInputStore()
-help_ = OptionallyLatentString(HELP_HEADER + indent(HOTWORDS_HELP, "   "))
-config = OptionallyLatentString(HELP_HEADER + indent(CONFIGURATION_INFO, "   "))
-
-
-# the following functions affect state
-def toggle_index():
-    """Show/hide the index of transformed items in the repl."""
-    user_inputs.show_index = not user_inputs.show_index
-
-
-def clear_list():
-    """Clear all transformations from the repl."""
-    user_inputs.clear()
-
-
-def drop_list_item(*indexes):
-    """Drop the value at any of the specifed indexes.
-    Drop [-1] if no index provided."""
-    if not indexes:
-        user_inputs.pop()
-        return
-
-    user_inputs._working_list = [
-        item
-        for idx, item in enumerate(user_inputs._working_list)
-        if str(idx) not in indexes
-    ]
-
-
-def append_to_list(item):
-    """Add an item to the store for transformation."""
-    user_inputs.append(item)
-
-
-def quit():
-    """Leave the repl.
-
-    As of this moment, state needs to be cleaned up on exit in service of testing.
-    During the lifecycle of test invocation, the state variables stick around across
-    runs for some reason."""
-    user_inputs.clear()
-    help_.latent = True
-    config.latent = True
-    raise typer.Exit
-
-
-def show_help():
-    """Make help visible by toggling the latent property of `help_`."""
-    help_.latent = not help_.latent
-
-
-def show_config():
-    """Make your configuration visible by toggling the latent property on `config`."""
-    config.latent = not config.latent
-
-
-def go_back():
-    """Make all latent strings latent again so they do not show on screen."""
-    for string in [config, help_]:
-        string.latent = True
-
-
-def copy_to_clipboard():
-    """Copy all visible conversion results to the clipboard."""
-    values = "\n".join(map(flip_time_format, user_inputs._working_list))
-    pyperclip.copy(values)
-    typer.echo(
-        f"Converted date{'s' if len(user_inputs) > 1 else ''} copied to clipboard."
-    )
-    quit()
-
-
-dispatch = make_dispatcher(
-    [RESET_HOTWORDS, clear_list],
-    [DROP_HOTWORDS, drop_list_item],
-    [TOGGLE_INDEX_HOTWORDS, toggle_index],
-    [EXIT_HOTWORDS, quit],
-    [HELP_HOTWORDS, show_help],
-    [SHOW_CONFIG_HOTWORDS, show_config],
-    [GO_BACK_HOTWORDS, go_back],
-    [COPY_HOTWORDS, copy_to_clipboard],
-    default=append_to_list,
-)
 
 
 @app.command(
@@ -130,14 +30,11 @@ def repl(tz: str = "America/Chicago"):
     clear()  # create a full-screen view
 
     while True:
-        visible_content = next(
-            (x for x in [help_, config, user_inputs] if len(x)), NO_TIMES_YET_MESSAGE
-        )
         try:
             input_ = typer.prompt(
                 "",
                 prompt_suffix=colored_prompt,  # suffix lookin like a prefix
-                default=visible_content,
+                default=visible_content(),
                 show_default=True,
             )
         except typer.Abort:
